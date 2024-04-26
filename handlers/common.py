113fd8b8855bf6200044bdcdd4c5906b5fc28b63
troubleShooter239 <morgun2282@gmail.com>
2024-04-26 10:43:05 +0300

    add params to api request

--- handlers/common.py
+++ handlers/common.py
@@ -3,6 +3,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from modules.requests.weather_api import WeatherAPI
+from utils.config import API_KEY, OPEN_WEATHER_API_URL
 
 
 common_router = Router()
@@ -17,7 +18,13 @@ async def weather_command_handler(message: Message):
     """This handler receives messages with `/weather` command"""
     try:
         await message.answer(
-            await WeatherAPI.get_weather(message.text.split()[1])
+            await WeatherAPI.get_weather(
+                OPEN_WEATHER_API_URL,
+                {'q': message.text.split()[1],
+                 'app_id': API_KEY,
+                 'units': 'metric',}
+            )
         )
-    except Exception:
-        await message.answer("Error with receiving weather forecast")
+    except Exception as e:
+        print(e)
+        await message.answer("Error with receiving weather forecast.")
--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 # TODO: Take tokens from env variable
-API_KEY = ''
-BOT_TOKEN = ''
+API_KEY = 'd5ddb9bfc7bb6da69fe5d5f75e98dfae'
+BOT_TOKEN = '7161877732:AAGuziVM4qGpeBpGha_oYu9i2QXBEcBpenI'
 
-OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+OPEN_WEATHER_API_URL = f'https://api.openweathermap.org/data/2.5/weather'
