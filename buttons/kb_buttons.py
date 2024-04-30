0c972813139abafd650890857f7fb2300a7a9c43
mnogaBukav <lilianna040@gmail.com>
2024-04-30 13:33:40 +0300

    add cancel_btn and playing games func

--- buttons/kb_buttons.py
+++ buttons/kb_buttons.py
@@ -1,11 +1,30 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-weather_btn = KeyboardButton(text="Weather")
-user_geo_btn = KeyboardButton(text="My current location", 
-                              request_location=True)
+cancel_btn = KeyboardButton(text='Cancel')
+weather_btn = KeyboardButton(text='Weather')
+user_geo_btn = KeyboardButton(
+    text='My current location', request_location=True
+)
+
+play_dice_btn = KeyboardButton(text='Play game')
+dice_btn = KeyboardButton(text='🎲')
+dart_btn = KeyboardButton(text='🎯')
+basketball_btn = KeyboardButton(text='🏀')
+football_btn = KeyboardButton(text='⚽') 
+bowling_btn = KeyboardButton(text='🎳')
+slots_btn = KeyboardButton(text='🎰')
+
+games_markup = ReplyKeyboardMarkup(keyboard=[
+    [dice_btn, dart_btn, basketball_btn],
+    [football_btn, bowling_btn, slots_btn],
+    [cancel_btn]
+], resize_keyboard=True)
 
 start_markup = ReplyKeyboardMarkup(keyboard=[
-    [weather_btn]
+    [weather_btn, play_dice_btn],
 ], resize_keyboard=True)
-weather_markup = ReplyKeyboardMarkup(keyboard=[[user_geo_btn]],
-                                     resize_keyboard=True)
+
+weather_markup = ReplyKeyboardMarkup(
+    keyboard=[[user_geo_btn, cancel_btn]], 
+    resize_keyboard=True
+)
--- handlers/common.py
+++ handlers/common.py
@@ -3,7 +3,7 @@ from aiogram.filters import CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from buttons.kb_buttons import start_markup, weather_btn, weather_markup
+from buttons.kb_buttons import cancel_btn, start_markup, weather_btn, weather_markup, play_dice_btn, games_markup
 from modules.requests.async_http_client import AsyncHTTPClient
 from states.current_weather import CurrentWeather
 
@@ -12,13 +12,22 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
+async def command_start_handler(msg: Message) -> None:
     """This handler receives messages with `/start` command"""
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!", 
+    await msg.answer(f"Hello, {html.bold(msg.from_user.full_name)}!", 
                          reply_markup=start_markup)
 
+@router.message(F.text == cancel_btn.text)
+async def cancel_handler(msg: Message, state: FSMContext):
+    await msg.answer("Menu", reply_markup=start_markup)
+    await state.clear()
+
+@router.message(StateFilter(None), F.text == play_dice_btn.text)
+async def play_dice_handler(msg: Message):
+    await msg.answer("What game do you want to play?", reply_markup=games_markup)
+
 @router.message(StateFilter(None), F.text == weather_btn.text)
-async def weather_command_handler(message: Message, state: FSMContext):
+async def weather_command_handler(msg: Message, state: FSMContext):
     """This handler receives messages with `/weather` command"""
-    await message.answer("Choose location:", reply_markup=weather_markup)
+    await msg.answer("Choose location:", reply_markup=weather_markup)
     await state.set_state(CurrentWeather.choosing_cur_geo_or_city_name)
