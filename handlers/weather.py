8c7e6b5b1d8e8ca0b933189dd47c1f4d382a9c2f
troubleShooter239 <morgun2282@gmail.com>
2024-05-05 17:54:14 +0300

    add objects to handlers by DI

--- handlers/weather.py
+++ handlers/weather.py
@@ -3,31 +3,32 @@ from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
 from buttons.kb_buttons import start_markup
-from handlers.common import http_session
-from modules.weather.open_weather_client import OpenWeatherClient
+from modules.weather_client import WeatherClient
 from states.current_weather import CurrentWeather
 from utils.config import API_KEY, OPEN_WEATHER_API_URL
 
+
 router = Router()
-weather_client = OpenWeatherClient(http_session) 
 
 @router.message(CurrentWeather.choosing_cur_geo_or_city_name)
-async def handle_city(msg: Message, state: FSMContext):
-    data = {'appid': API_KEY, 'units': 'metric'}
+async def handle_city(msg: Message, 
+                      state: FSMContext, 
+                      weather_client: WeatherClient):
     if msg.location:
         location = {'lat': str(msg.location.latitude),
                     'lon': str(msg.location.longitude)}
     else:
         text_list = msg.text.split(',')
         if len(text_list) == 2:
-            location = {'lat': str(text_list[0]),
+            location = {'lat': str(text_list[0]),   
                         'lon': str(text_list[1])}
         else:
             location = {'q': text_list[0]}
+    
+    data = {'appid': API_KEY, 'units': 'metric'}
     data.update(location)
-
     await msg.answer(
-        await weather_client.get_current_weather(OPEN_WEATHER_API_URL, data),
+        await weather_client.get_current(OPEN_WEATHER_API_URL, data),
         reply_markup=start_markup
     )
     await state.clear()
--- main.py
+++ main.py
@@ -3,23 +3,33 @@ from asyncio import run
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiohttp import ClientSession
 
 from handlers import common, weather
+from modules.weather_client import WeatherClient
 from utils.config import BOT_TOKEN
 
 
 async def main() -> None:
     bot = Bot(token=BOT_TOKEN, 
               default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
-    dp.include_routers(
-        common.router, weather.router,
+    
+    client_session = ClientSession()
+    dp = Dispatcher(
+        client_session=client_session,
+        weather_client=WeatherClient(client_session),
     )
+    
+    dp.include_routers(
+        common.router, 
+        weather.router,
+    )   
+    
     try:
         await dp.start_polling(bot)
     finally:
-        await common.http_session.close()
+        await client_session.close()
 
 
 if __name__ == '__main__':
-    run(main())
+    run(main()) 
